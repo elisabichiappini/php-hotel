c920b13515a71dbbf45259d87a27fd11cf921042
elisabichiappini <elisabichiappini@gmail.com>
2024-01-09 21:30:04 +0100

    feat: import table bootstrap

--- index.php
+++ index.php
@@ -54,15 +54,45 @@
         <h1>Hotels</h1>
     </header>
     <div class="container">
-        <?php foreach($hotels as $index => $hotel) {
-            echo $index;
-            foreach($hotel as $key => $value){
-                echo "<br>";
-                echo "$key: $value";
-            };    
-            echo "<hr>";        
-            };
-        ?>
+        
+        <table class="table">
+            <?php 
+                foreach($hotels as $index => $hotel) {
+                    echo $index;
+                    foreach($hotel as $key => $value){
+                        echo "$key: $value";
+                    };          
+                    echo "<hr>";
+                };
+            ?>
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">First</th>
+                    <th scope="col">Last</th>
+                    <th scope="col">Handle</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th scope="row">1</th>
+                    <td>Mark</td>
+                    <td>Otto</td>
+                    <td>@mdo</td>
+                </tr>
+                <tr>
+                    <th scope="row">2</th>
+                    <td>Jacob</td>
+                    <td>Thornton</td>
+                    <td>@fat</td>
+                </tr>
+                <tr>
+                    <th scope="row">3</th>
+                    <td colspan="2">Larry the Bird</td>
+                    <td>@twitter</td>
+                </tr>
+            </tbody>
+        </table>
     </div>
 </body>
 </html>
