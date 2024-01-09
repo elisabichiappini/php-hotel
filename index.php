7de3b79c63f91b042d2283d315111c9c911562fa
elisabichiappini <elisabichiappini@gmail.com>
2024-01-09 22:46:10 +0100

    feat: creating table

--- index.php
+++ index.php
@@ -54,44 +54,24 @@
         <h1>Hotels</h1>
     </header>
     <div class="container">
-        
         <table class="table">
-            <?php 
-                foreach($hotels as $index => $hotel) {
-                    echo $index;
-                    foreach($hotel as $key => $value){
-                        echo "$key: $value";
-                    };          
-                    echo "<hr>";
-                };
-            ?>
             <thead>
-                <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">First</th>
-                    <th scope="col">Last</th>
-                    <th scope="col">Handle</th>
-                </tr>
+                <th>
+                    <?php 
+                    foreach($hotels as $index => $hotel) {
+                        echo $index;
+                    ?>
+                </th>
             </thead>
             <tbody>
-                <tr>
-                    <th scope="row">1</th>
-                    <td>Mark</td>
-                    <td>Otto</td>
-                    <td>@mdo</td>
-                </tr>
-                <tr>
-                    <th scope="row">2</th>
-                    <td>Jacob</td>
-                    <td>Thornton</td>
-                    <td>@fat</td>
-                </tr>
-                <tr>
-                    <th scope="row">3</th>
-                    <td colspan="2">Larry the Bird</td>
-                    <td>@twitter</td>
-                </tr>
+                <?php
+                foreach($hotel as $key => $value) {
+                    echo "<td>$key</td>";
+                    echo "<td><strong>$value</strong></td>";
+                }
+            };?>
             </tbody>
+            <!--intestazione tabella -->
         </table>
     </div>
 </body>
