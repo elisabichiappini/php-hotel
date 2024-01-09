c7895e9ad9f64c1bae687f96e6dc8706872d00fd
elisabichiappini <elisabichiappini@gmail.com>
2024-01-09 18:57:07 +0100

    feat: print datas array

---
+++ prova.php
@@ -0,0 +1,24 @@
+<?php
+
+$array = [
+    'elisa', 'sara', 'anna'
+];
+
+echo $array['2']; //anna
+echo"<hr>";
+
+var_dump($array) ; //stampa tutto l'array
+echo"<hr>";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
