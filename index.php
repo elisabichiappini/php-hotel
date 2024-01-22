004220c4bda02d9de4d7f945cde86837bc9fd245
elisabichiappini <elisabichiappini@gmail.com>
2024-01-22 17:00:55 +0100

    feat: select in parking

--- index.php
+++ index.php
@@ -37,6 +37,22 @@
             'distance_to_center' => 50
         ],
     ];
+
+    // copia dati in variabile di appoggio
+    $filters_hotel = $hotels;
+
+    //filter con parcheggio
+    if(!empty($_GET['parking'])){
+        $parking = $_GET['parking'] === 'yes' ? true : false;
+
+        $temp_hotels = [];
+        foreach($filters_hotel as $hotel) {
+            if($hotel['parking'] === $parking) {
+                $temp_hotels[] = $hotel;
+            }
+        }
+        $filters_hotel = $temp_hotels;
+    }
 ?>
 
 <!DOCTYPE html>
@@ -47,7 +63,7 @@
     <title>PHP hotel</title>
     <!--file CDN Bootstrap-->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <!--file CDN Bootstrap-->
+    <!--/file CDN Bootstrap-->
 </head>
 <body class="bg-dark text-center">
     <!--intestazione-->
@@ -58,24 +74,41 @@
     <!--corpo-->
     <main class="container">
         <!--form-->
-        <form class="row g-3 pb-3 align-items-center d-flex justify-content-center" action="#" method="GET">
-            <div class="col-4">
-                <label class="visually-hidden" for="voto">Filtro1</label>
-                <div class="input-group">
-                <div class="input-group-text">Filtro Hotel</div>
-                <input type="text" class="form-control" id="voto" placeholder="Voto da 1 a 5">
+        <form action="index.php" method="GET">
+            <div class="row g-3 pb-3 align-items-center d-flex justify-content-center">
+                <!--filtro select parcheggio-->
+                <div class="col-auto">
+                    <label class="visually-hidden" for="parking">Filtro2</label>
+                    <select class="form-select" id="parking" name="parking">
+                        <option value="">Parking Filter</option>
+                        <option value="yes">Yes</option>
+                        <option value="no">No</option>
+                    </select>
                 </div>
-            </div>
-            <div class="col-auto">
-                <label class="visually-hidden" for="parking">Filtro2</label>
-                <select class="form-select" id="parking">
-                    <option selected>Parking car</option>
-                    <option value="1">Yes</option>
-                    <option value="2">No</option>
-                </select>
-            </div>
-            <div class="col-auto">
-                <button type="submit" class="btn btn-primary">Submit</button>
+                <!--/filtro select parcheggio-->
+                <!--filtro select voto-->
+                <div class="col-auto">
+                    <label for="voto"  class="visually-hidden"></label>
+                    <select class="form-select" id="voto" name="voto">
+                        <option selected value="">Vote All</option>
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="4">4</option>
+                        <option value="5">5</option>
+                        <!-- alternative option vote
+                        <?php for($i = 1; $i <=5; $i++): ?>
+                            <option value="<?php echo $i;?>"><?php echo $i;?></option>
+                        <?php endfor; ?> -->
+                        <!-- /alternative option vote-->
+                    </select>
+                </div>
+                <!--/filtro input voto-->
+                <!--bottone-->
+                <div class="col-auto">
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </div>
+                <!--/bottone-->
             </div>
         </form>
         
@@ -91,17 +124,17 @@
                 </tr>
             </thead>
             <tbody>
-                <!--creazione di records per gli hotel in tabella-->
-                <?php foreach($hotels as $hotel) :?>
-                    <tr>
-                        <td><?php echo $hotel['name'];?></th>
-                        <td><?php echo $hotel['description'] ;?></td>
-                        <td><?php echo $hotel['parking']=== true ? 'Yes' : 'No';?></td>
-                        <td><?php echo $hotel['vote']; ?></td>
-                        <td><?php echo $hotel ['distance_to_center']; ?>km</td>
-                    </tr>
-                <?php endforeach;?>
-                <!--/creazione di records per gli hotel in tabella-->
+            <!--creazione di records per gli hotel in tabella-->
+            <?php foreach ($filters_hotel as $hotel) :?>
+                <tr>
+                    <td><?php echo $hotel['name'];?></th>
+                    <td><?php echo $hotel['description'] ;?></td>
+                    <td><?php echo $hotel['parking'];?></td>
+                    <td><?php echo $hotel['vote']; ?></td>
+                    <td><?php echo $hotel ['distance_to_center']; ?>km</td>
+                </tr>
+            <?php endforeach; ?>
+            <!--/creazione di records per gli hotel in tabella-->
             </tbody>
         </table>
         <!--/dati stampati in tabella-->
